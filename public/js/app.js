553785c29e485154a15414c60458ef56428249d7
Diego Martin Marmol <diegomartinmarmol@gmail.com>
2018-11-12 21:13:40 -0300

    Adding Class 06 and practica.rar file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 PR.prettyPrint();
+
+var items = document.querySelectorAll('[class*="example-"] .item');
+
+items.forEach(function(item) {
+	var wrapper = document.createElement("div");
+	wrapper.classList.add("item-wrapper");
+	item.parentNode.insertBefore(wrapper, item);
+	wrapper.appendChild(item);
+});
